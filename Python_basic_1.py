c391fdb31425b3597520c37afa1294c5207aca70
Sandrainiobong <133987540+Sandrainiobong@users.noreply.github.com>
2023-05-18 16:08:33 +0100

    Create Python_basic_1.py

---
+++ Python_basic_1.py
@@ -0,0 +1,7 @@
+name=input("what is your name\t")
+
+age=int(input("how old are you?\t"))
+
+tele=int(input("Enter your phone number\t"))
+
+print("Hello {}, you are {} years old with the phone number {}".format(name,age,tele))
