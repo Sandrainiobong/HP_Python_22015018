b9b9256736fdd21b36ecf004f17aa7c123689aad
Sandrainiobong <133987540+Sandrainiobong@users.noreply.github.com>
2023-05-18 16:13:58 +0100

    Create Python_basic_2.py

---
+++ Python_basic_2.py
@@ -0,0 +1,9 @@
+a=int(input("enter your first number:\t"))
+
+b=int(input("enter the second number:\t"))
+
+a,b=b,a
+
+print("value of fist number after swapping is: {}".format(a))
+
+print("value of second number after swapping is:{}".format(b))
